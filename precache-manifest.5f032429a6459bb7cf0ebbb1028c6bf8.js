16b6bae82e45f28e77ff4764970bd0734e53f996
pureworldnew <pureworldnew@gmail.com>
2021-03-10 10:48:28 +0800

    Updates

---
+++ precache-manifest.5f032429a6459bb7cf0ebbb1028c6bf8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23e63f282b9d5e202949f64f89202cca",
+    "url": "/personal-react-web/index.html"
+  },
+  {
+    "revision": "cd8ae7157ec661276bfd",
+    "url": "/personal-react-web/static/css/main.31f4fbea.chunk.css"
+  },
+  {
+    "revision": "7af55f6940efdd380cad",
+    "url": "/personal-react-web/static/js/2.43e45031.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-react-web/static/js/2.43e45031.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd8ae7157ec661276bfd",
+    "url": "/personal-react-web/static/js/main.94ecb24e.chunk.js"
+  },
+  {
+    "revision": "8f08c6bbd1bede242e5a",
+    "url": "/personal-react-web/static/js/runtime-main.6d2c3e14.js"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/personal-react-web/static/media/github_icon.76d344f0.svg"
+  },
+  {
+    "revision": "b421b93a33a6fdae029c32d6945af310",
+    "url": "/personal-react-web/static/media/linkedln_icon.b421b93a.png"
+  },
+  {
+    "revision": "04ba7db2ec88f1d50e0b518ef95c504e",
+    "url": "/personal-react-web/static/media/new.04ba7db2.png"
+  },
+  {
+    "revision": "499bc679a93d7fd8d0c47ec1beb0ca58",
+    "url": "/personal-react-web/static/media/nick_lin_resume.499bc679.pdf"
+  }
+]);
